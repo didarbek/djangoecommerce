712599cc61e8bd318c39a68eb9b90cf53cd44430
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-02-08 16:09:02 +0400

    first commit

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('detail/',views.product_page,name='product_page'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def product_page(request):
+    return render(request,'product-page.html')
