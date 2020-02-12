21a53144f97ed8a45eee52948c3f3311dd698bbe
khaldon <mohamed.khaled33388@gmail.com>
2020-02-12 15:25:23 +0200

    update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Item, Category
 # Register your models here.
+
+@admin.register(Category)
+class CategoryProfile(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug':('name',)}
+
+
+
+admin.site.register(Item)
