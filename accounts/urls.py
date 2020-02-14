848984e29a3f31c6305dd1ae15ca81a3e0b16046
khaldon <mohamed.khaled33388@gmail.com>
2020-02-14 13:55:34 +0200

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('signup/',views.register,name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('accounts:password_change_done')), name='password_change'),
     path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view( success_url=reverse_lazy('accounts:password_reset_done')),  name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
