45d028969a139915417ada5befed306acf5e0ff0
khaldon <mohamed.khaled33388@gmail.com>
2020-02-16 21:05:58 +0200

    update

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,8 @@ app_name = 'core'
 
 urlpatterns = [
     path('',views.ItemList.as_view(),name='home'),
+    path('men/', views.MenItemList.as_view(), name='men_form'),
+    path('women/', views.WomenItemList.as_view(), name='women_form'),
     path('detail/<int:pk>/',views.product_detail,name='product_detail'),
     path('cart/',views.OrderSummaryView.as_view(),name='cart'),
     path('add-to-cart/<int:pk>/',views.add_to_cart,name='add-to-cart'),
